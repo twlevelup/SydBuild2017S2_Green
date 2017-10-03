2ad30bff21c895d0d049b7342b6233ad075350ed
Tammeryy <TammyXQZ@yahoo.com>
2017-10-03 20:46:56 +1100

    Added console log for emergency confirmation

--- src/app/pages/EmergencyScreen/EmergencyScreen.jsx
+++ src/app/pages/EmergencyScreen/EmergencyScreen.jsx
@@ -7,7 +7,7 @@ export const EmergencyScreenComponent = () => {
   return (
     <div id='emergency-screen-page'>
       <p id='emergency-question'>
-          Would you like to contact emergency?
+        Would you like to contact emergency?
       </p>
       <div className='no-button button-container'>
         <span> No </span>
@@ -21,9 +21,32 @@ export const EmergencyScreenComponent = () => {
   );
 };
 
+// This code can be used to retrieve location when the website
+// moves to HTTPS
+/*
+function showPosition(position) {
+  console.log('Latitude: ', position.coords.latitude);
+  console.log('Longitude: ', position.coords.longitude);
+}
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log('Geolocation is not supported by this browser.');
+  }
+}
+*/
+export const logAndNavigateToConfirmation = () => {
+  ButtonAction.goToPage('/emergency-confirmation');
+  console.log('Latitude: ', -33.8629344);
+  console.log('Longitude: ', 151.20858810000004);
+  console.log('TimeStamp: ', new Date(Date.now()).toLocaleString());
+};
+
 export const EmergencyScreenButtons = {
   LEFT: () => ButtonAction.goToPage('/'),
-  SCREEN: () => ButtonAction.goToPage('/emergency-confirmation'),
+  SCREEN: () => logAndNavigateToConfirmation(),
 };
 
 export default WithButtonConfigs(EmergencyScreenComponent, EmergencyScreenButtons);
