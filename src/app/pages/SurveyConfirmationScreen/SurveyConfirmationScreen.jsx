6001ccc083a89972d6c7efe585c45bbea68143f5
Jeremy Wong <navhaxs@hotmail.com>
2017-10-14 13:06:54 +1100

    [Jessie/Tammy]Styleing Survey response confirmation page

--- src/app/pages/SurveyConfirmationScreen/SurveyConfirmationScreen.jsx
+++ src/app/pages/SurveyConfirmationScreen/SurveyConfirmationScreen.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ButtonAction from '../../../framework/util/ButtonAction';
 import WithButtonConfigs from '../../../framework/containers/WithButtonConfigs';
+import './survey_confirmation_screen.css';
 
 export const SurveyConfirmationScreenComponent = () => {
   return (
     <div id='survey-confirmation-screen-page'>
-      <h2>Your responses have been recorded</h2>
-      <h1>Thank you</h1>
+      <h2 className='confirmMessage'>Your responses have been recorded</h2>
+      <h1 className='thankYouMessage'>Thank you</h1>
     </div>
   );
 };
